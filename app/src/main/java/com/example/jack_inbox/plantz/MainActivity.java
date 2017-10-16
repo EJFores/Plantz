3758068dcee7f3fe2e68b7febaad2b87acfe5433
Manuel A. Bolanos-Tapia <32848231+aosmannybolanos@users.noreply.github.com>
2017-10-16 15:57:50 -0500

    \\Added Manny's Flowery Menu \\Removed Junk

--- app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = (Button) findViewById(R.id.runTests);
+        final Button button = (Button) findViewById(R.id.runtest);
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
