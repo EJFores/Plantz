e19edec83ce52da645e0d2a0d24f61ff3e228036
Manuel A. Bolanos-Tapia <32848231+AOSMannyBolanos@users.noreply.github.com>
2017-10-16 12:25:54 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.jack_inbox.plantz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//Ethan Sucks Bums
 public class MainActivity extends AppCompatActivity {
 
     @Override
