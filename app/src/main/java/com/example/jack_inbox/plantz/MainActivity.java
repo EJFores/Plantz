e9f935343e1b0d2dd6aa2849a83338d14bcbde7a
Manuel A. Bolanos-Tapia <32848231+AOSMannyBolanos@users.noreply.github.com>
2017-10-16 12:28:06 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
@@ -1,8 +1,9 @@
+//Start of Project
 package com.example.jack_inbox.plantz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//Ethan Sucks Bums of Hairy Dwarven Women
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
