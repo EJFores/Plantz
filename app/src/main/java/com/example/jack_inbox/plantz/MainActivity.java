883ba17443fbfed029cfc1b1d47c38d0903a4be9
SIU851975608 <ethanatflores@gmail.com>
2017-10-16 15:43:35 -0500

    Groundwork for Testing Activity \\Added new activity for Testing Class \\Changed android manfest for rotation and new activity \\Removed traitous acts by computer.

--- app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
@@ -1,14 +1,29 @@
 //Start of Project
 package com.example.jack_inbox.plantz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.runTests);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(MainActivity.this, MainTesting.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/jack_inbox/plantz/MainTesting.java
@@ -0,0 +1,15 @@
+package com.example.jack_inbox.plantz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainTesting extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
