a3d795b6327dc73cabb30956cf0cf60d4fb7a762
EJFores <32749043+EJFores@users.noreply.github.com>
2017-10-16 12:27:39 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.jack_inbox.plantz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//Ethan Sucks Bums
+//Ethan Sucks Bums of Hairy Dwarven Women
 public class MainActivity extends AppCompatActivity {
 
     @Override
