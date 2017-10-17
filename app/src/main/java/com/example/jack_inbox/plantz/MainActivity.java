fdaeabeeab30c09391863f4d005192dd547bd6ec
EJFores <32749043+ejfores@users.noreply.github.com>
2017-10-17 15:03:12 -0500

    \\Added Activity for API Lookup

--- app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
@@ -36,6 +36,14 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        final Button buttonapi = (Button) findViewById(R.id.api);
+        buttonapi.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, APILookupActivity.class));
+            }
+        });
 
     }
 }
