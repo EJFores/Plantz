9c372ba0873bd0daddba091df01dc266295d6b5d
gregevans <greaper2513@gmail.com>
2017-10-18 21:13:08 -0500

    added some temp buttons and things to the fragments, it crashes when the "test run"  button is pressed, not entirely sure what it is
    I changed the activity_testing.xml to activity_temp.xml to see if it was the fragmented activity or not.  Not entirely sure what causes the crash, maybe someone else can take a look

--- app/src/main/java/com/example/jack_inbox/plantz/MapsActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MapsActivity.java
@@ -1,8 +1,13 @@
 package com.example.jack_inbox.plantz;
 
-/**
- * Created by gregevans on 10/18/17.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MapsActivity {
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
 }
--- app/src/main/java/com/example/jack_inbox/plantz/WeatherActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/WeatherActivity.java
@@ -1,8 +1,13 @@
 package com.example.jack_inbox.plantz;
 
-/**
- * Created by gregevans on 10/18/17.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class WeatherActivity {
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
 }
