20b5680a2448c76071b4289b775e2aa7b728ddba
gregevans <greaper2513@gmail.com>
2017-10-19 14:26:22 -0500

    added some temp buttons and things to the fragments, it crashes when the "test run"  button is pressed, not entirely sure what it is
    I changed the activity_testing.xml to activity_temp.xml to see if it was the fragmented activity or not.  Not entirely sure what causes the crash, maybe someone else can take a look

--- app/src/main/java/com/example/jack_inbox/plantz/MainTesting.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainTesting.java
@@ -10,6 +10,6 @@ public class MainTesting extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_temp);
+        setContentView(R.layout.activity_testing);
     }
 }
