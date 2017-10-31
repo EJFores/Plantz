5bf65b1d6eef992545086069d5a75516146eb05e
gregevans <greaper2513@gmail.com>
2017-10-30 22:06:12 -0500

    tried connect to an api and seeing if the data would output, it does, but I've tried with different api's, and I can't connect to them properly.
    The one I'm connected is temporary and for testing if i can retrieve the data.

---
+++ app/src/main/java/com/example/jack_inbox/plantz/FetchPlantData.java
@@ -0,0 +1,52 @@
+package com.example.jack_inbox.plantz;
+
+
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by gregevans on 10/30/17.
+ */
+
+public class FetchPlantData extends AsyncTask<Void, Void, Void> {
+    String data = "";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://data.cityofchicago.org/api/views/i8y3-ytj4/rows.json?accessType=DOWNLOAD");
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        APILookupActivity.data.setText(this.data);
+    }
+}
