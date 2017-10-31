c6fab769b7b6f6f902ddcfa144bba23c0d026652
gregevans <greaper2513@gmail.com>
2017-10-30 22:05:29 -0500

    tried connect to an api and seeing if the data would output, it does, but I've tried with different api's, and I can't connect to them properly.
    The one I'm connected is temporary and for testing if i can retrieve the data.

--- app/src/main/java/com/example/jack_inbox/plantz/APILookupActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/APILookupActivity.java
@@ -1,15 +1,37 @@
 package com.example.jack_inbox.plantz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 
 public class APILookupActivity extends AppCompatActivity
 {
+    Button click;
+   public static TextView data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apilook);
+
+
+        click = (Button) findViewById(R.id.button);
+        data = (TextView) findViewById(R.id.fetchdata);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                FetchPlantData process = new FetchPlantData();
+                process.execute();
+
+            }
+
+        });
+
+
     }
 }
 //thing
