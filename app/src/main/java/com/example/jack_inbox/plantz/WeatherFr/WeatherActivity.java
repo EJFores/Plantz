d260e3935ce671b3614cbb1f02631c760146eca5
Manuel A. Bolanos-Tapia <32848231+AOSMannyBolanos@users.noreply.github.com>
2017-11-15 13:56:49 -0600

    Update WeatherActivity.java
    MESSED EVERYTHING UP BUT CHANGED 4 TO A 6 explosions

--- app/src/main/java/com/example/jack_inbox/plantz/WeatherFr/WeatherActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/WeatherFr/WeatherActivity.java
@@ -31,60 +31,60 @@ public class WeatherActivity extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        Log.d("Fragment 4", "onAttach");
+        Log.d("Fragment 6", "onAttach");
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("Fragment 4", "onCreate");
+        Log.d("Fragment 6", "onCreate");
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Log.d("Fragment 4", "onActivityCreated");
+        Log.d("Fragment 6", "onActivityCreated");
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        Log.d("Fragment 4", "onStart");
+        Log.d("Fragment 6", "onStart");
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Log.d("Fragment 4", "onResume");
+        Log.d("Fragment 6", "onResume");
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d("Fragment 4", "onPause");
+        Log.d("Fragment 6", "onPause");
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.d("Fragment 4", "onStop");
+        Log.d("Fragment 6", "onStop");
 
     }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        Log.d("Fragment 4", "onDestroyView");
+        Log.d("Fragment 6", "onDestroyView");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("Fragment 4", "onDestroy");
+        Log.d("Fragment 6", "onDestroy");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        Log.d("Fragment 4", "onDetach");
+        Log.d("Fragment 6", "onDetach");
     }
 }
