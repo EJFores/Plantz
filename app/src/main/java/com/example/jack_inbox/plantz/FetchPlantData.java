de53e998fbb5171723824dc7ea5a6d60119dad53
gregevans <greaper2513@gmail.com>
2017-11-16 00:14:59 -0600

    uh

--- app/src/main/java/com/example/jack_inbox/plantz/FetchPlantData.java
+++ app/src/main/java/com/example/jack_inbox/plantz/FetchPlantData.java
@@ -22,31 +22,50 @@ import java.net.URL;
 
 public class FetchPlantData extends AsyncTask<Void, Void, Void> {
     String data = "";
+    String dataParsed = "";
+    String singleParsed = "";
+
     @Override
     protected Void doInBackground(Void... voids) {
         try {
-            URL url = new URL("https://plantsdb.xyz/search?fields=Genus");
-
+            URL url = new URL("https://api.myjson.com/bins/j5f6b");
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line = "";
-
             while(line != null){
                 line = bufferedReader.readLine();
                 data = data + line;
             }
+
+            JSONArray JA = new JSONArray(data);
+            for(int i =0 ;i <JA.length(); i++){
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed =  "Name:" + JO.get("name") + "\n"+
+                        "Password:" + JO.get("password") + "\n"+
+                        "Contact:" + JO.get("contact") + "\n"+
+                        "Country:" + JO.get("country") + "\n";
+
+                dataParsed = dataParsed + singleParsed +"\n";
+
+
+            }
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+
         return null;
     }
+
 
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        APILookupActivity.data.setText(this.data);
+        APILookupActivity.data.setText(this.dataParsed);
     }
 }
