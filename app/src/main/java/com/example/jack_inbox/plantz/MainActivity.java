fbf0da50db0e0f85a3a381384ab29fa6fc4f32f2
SIU851975608 <ethanatflores@gmail.com>
2017-11-30 15:02:18 -0600

    BEHOLD THE POWER OF SLIDESHOW (also known as powerpoint XL) -powered by EA

--- app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                startActivity(new Intent(MainActivity.this, MainTesting.class));
+                startActivity(new Intent(MainActivity.this, WeatherActivity.class));
             }
         });
 
--- app/src/main/java/com/example/jack_inbox/plantz/MapsActivity.java
+++ app/src/main/java/com/example/jack_inbox/plantz/MapsActivity.java
@@ -1,8 +1,10 @@
-package com.example.jack_inbox.plantz.MapsFr;
+package com.example.jack_inbox.plantz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Camera;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Fragment;
 import android.util.Log;
@@ -21,12 +23,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 //https://stackoverflow.com/questions/19353255/how-to-put-google-maps-v2-on-a-fragment-using-viewpager
 //my precious....my precious.....THIEF THIEF! WE STOLES IT, WE STOLES IT FROM THEM. golem golem...
-public class MapsActivity extends Fragment
+public class MapsActivity extends AppCompatActivity
 {
     MapView mMapView;
     private GoogleMap googleMap;
 
-    @Override
+    private GestureDetectorCompat gestureObject;
+
+    /**@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View rootView =  inflater.inflate(R.layout.activity_testing, container, false);
@@ -58,66 +62,48 @@ public class MapsActivity extends Fragment
             }
         });
         return rootView;
+    }**/
+
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_mapsact);
+
+            gestureObject = new GestureDetectorCompat(this, new LearnGesture());
+        }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureObject.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            Log.d("Fragment 2", "onAttach");
-        }
+    class LearnGesture extends GestureDetector.SimpleOnGestureListener{
 
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            Log.d("Fragment 2", "onCreate");
-        }
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY){
 
-        @Override
-        public void onActivityCreated(Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
-            Log.d("Fragment 2", "onActivityCreated");
-        }
-
-        @Override
-        public void onStart() {
-            super.onStart();
-            Log.d("Fragment 2", "onStart");
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-            Log.d("Fragment 2", "onResume");
-        }
-
-        @Override
-        public void onPause() {
-            super.onPause();
-            Log.d("Fragment 2", "onPause");
-        }
-
-        @Override
-        public void onStop() {
-            super.onStop();
-            Log.d("Fragment 2", "onStop");
+            if(event2.getX() > event1.getX())
+            {
+                Intent intent = new Intent(
+                        MapsActivity.this, MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+            else if (event2.getX() < event1.getX())
+            {
+                Intent intent = new Intent(
+                        MapsActivity.this, DateActivity.class);
+                finish();
+                startActivity(intent);
+            }
+            return true;
 
         }
-        @Override
-        public void onDestroyView() {
-            super.onDestroyView();
-            Log.d("Fragment 2", "onDestroyView");
-        }
 
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            Log.d("Fragment 2", "onDestroy");
-        }
 
-        @Override
-        public void onDetach() {
-            super.onDetach();
-            Log.d("Fragment 2", "onDetach");
-        }
+    }
 
 }
